779e0a773edb2e15a9ad5d1e66260bbaf95a4c10
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-07-20 16:11:04 +0300

    Get input from user.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,9 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
-
-console.log(chalk.green.bold.inverse('Success!'));
+const command = process.argv[2]
+if (command === 'add') {
+  console.log("Adding note!");
+} else if (command === 'remove') {
+  console.log("Removing note!")
+}
