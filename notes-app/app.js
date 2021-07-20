32558ee608fb292d96672bfb43b975e0c5e8cfbb
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-07-20 15:39:20 +0300

    Print in colors.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,7 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const msg = getNotes();
 console.log(msg);
 
-console.log(validator.isEmail('monica@example.com'))
+console.log(chalk.green.bold.inverse('Success!'));
