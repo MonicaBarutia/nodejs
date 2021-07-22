c956592355f00b87ef0983f8fcfac44095245584
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-07-22 15:49:50 +0300

    Customize http request.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=c7c090f9923be6a9e8ca94dafb102757&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=c7c090f9923be6a9e8ca94dafb102757&query=37.8267,-122.4233&units=m'
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.current)
+request({ url: url, json:true }, (error, response) => {
+  const current = response.body.current
+  console.log(current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out." )
 })
