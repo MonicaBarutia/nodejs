d91ebbcbeb1e6d18aa90d20e82c0f57b1f2a5c15
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-02 13:04:37 +0300

    Code refactoring. Destructuring and Property Shorthand.

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,13 +3,14 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
   const url = 'http://api.weatherstack.com/current?access_key=c7c090f9923be6a9e8ca94dafb102757&query=' + latitude + ',' + longitude + '&units=m'
 
-  request({url: url, json: true}, (error, response) => {
+  request({url, json: true}, (error, { body } = {}) => {
+
     if (error) {
       callback("Unable to connect to weather service!")
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback("Unable to find location!")
     } else {
-      const current = response.body.current
+      const current = body.current
       callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out.")
     }
   })
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -3,16 +3,16 @@ const request = require('request')
 const geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibW9uaWI1IiwiYSI6ImNrcmV4MjI5OTBzZ2MzMW9lNGdzNTQ2dngifQ.nHNbhdm-GjPzyzRIhYQasQ&limit=1'
 
-  request({url: url, json: true}, (error, response) => {
+  request({url, json: true}, (error, { body } = {}) => {
     if (error) {
       callback("Unable to connect to location service!")
-    } else if (response.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       callback("Unable to find location. Please try again with another search term!")
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       })
     }
   })
