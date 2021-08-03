89cb29fee91915eb0be14d946c99bc809011eebc
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-03 11:43:16 +0300

    Dynamic pages with templating.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Monica'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about',{
+    title: 'About',
+    name: 'Monica'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    msg: 'For any problems visit this page.'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
