70524c52b1ac0480379f7a46dee5e6eb849463c8
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-03 19:06:48 +0300

    Add json http endpoint.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,11 +2,14 @@ const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
 
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath  = path.join(__dirname, '../templates/views')
+const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setuop handlebars engine and views location
@@ -25,7 +28,7 @@ app.get('', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.render('about',{
+  res.render('about', {
     title: 'About',
     name: 'Monica'
   })
@@ -40,16 +43,27 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
-  if(!req.query.address) {
+  if (!req.query.address) {
     return res.send({
       error: "You must provide an address!"
     })
   }
-  
-  res.send({
-    forecast: "Mostly sunny, temperature 25 degrees, real feel 23 degrees",
-    location: "Cluj Napoca",
-    address: req.query.address
+
+  geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+    if (error) {
+      return res.send({ error })
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error })
+      }
+      res.send({
+        location,
+        forecast: forecastData,
+        address: req.query.address
+      })
+    })
   })
 })
 
@@ -66,7 +80,7 @@ app.get('/products', (req, res) => {
   })
 })
 
-app.get('/help/*', (req,res) => {
+app.get('/help/*', (req, res) => {
   res.render('404', {
     error: "Help article not found.",
     title: "404 - Not found",
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,19 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=c7c090f9923be6a9e8ca94dafb102757&query=' + latitude + ',' + longitude + '&units=m'
+
+  request({url, json: true}, (error, { body } = {}) => {
+
+    if (error) {
+      callback("Unable to connect to weather service!")
+    } else if (body.error) {
+      callback("Unable to find location!")
+    } else {
+      const current = body.current
+      callback(undefined, current.weather_descriptions[0] + ". It is currently " + current.temperature + " degrees out. It feels like " + current.feelslike + " degrees out.")
+    }
+  })
+}
+
+module.exports = forecast
