c9c8dbc7579812d7ef1c46ed9bbf6ceff22ae953
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-06 14:54:33 +0300

    Insert documents.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -11,8 +11,53 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('users').insertOne({
-    name: 'Monica',
-    age: 25
+//   db.collection('users').insertOne({
+//     name: 'Monica',
+//     age: 25
+//   }, (error, result) => {
+//     if (error) {
+//       return console.log("Unable to insert user.")
+//     }
+//
+//     console.log(result.insertedId)
+//   })
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: "Jen",
+  //     age: 28
+  //   },
+  //   {
+  //     name: "John",
+  //     age: 27
+  //   },
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log("Unable to insert documents")
+  //   }
+  //
+  //   console.log(result.insertedIds)
+  // })
+
+  db.collection('tasks').insertMany([
+    {
+      description: 'Clean the house',
+      completed: true
+    },
+    {
+      description: 'Go grocery shopping',
+      completed: false
+    },
+    {
+      description: 'Do the dishes',
+      completed: true
+    }
+  ], (error, result) => {
+    if (error) {
+      return console.log("Unable to insert tasks.")
+    }
+
+    console.log(result.insertedIds)
   })
 })
+
