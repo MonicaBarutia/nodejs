244d3bf6adeb0ecca7c72f014bbbacea91c625ba
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-06 16:10:08 +0300

    The ObjectId.

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,8 +1,11 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectId } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectId()
+console.log(id.toHexString())
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
@@ -11,16 +14,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-//   db.collection('users').insertOne({
-//     name: 'Monica',
-//     age: 25
-//   }, (error, result) => {
-//     if (error) {
-//       return console.log("Unable to insert user.")
-//     }
-//
-//     console.log(result.insertedId)
-//   })
+  // db.collection('users').insertOne({
+  //   name: 'Maria',
+  //   age: 26
+  // }, (error, result) => {
+  //   if (error) {
+  //     return console.log("Unable to insert user.")
+  //   }
+  //
+  //   console.log(result.insertedId)
+  // })
 
   // db.collection('users').insertMany([
   //   {
@@ -39,25 +42,25 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   //   console.log(result.insertedIds)
   // })
 
-  db.collection('tasks').insertMany([
-    {
-      description: 'Clean the house',
-      completed: true
-    },
-    {
-      description: 'Go grocery shopping',
-      completed: false
-    },
-    {
-      description: 'Do the dishes',
-      completed: true
-    }
-  ], (error, result) => {
-    if (error) {
-      return console.log("Unable to insert tasks.")
-    }
-
-    console.log(result.insertedIds)
-  })
+  // db.collection('tasks').insertMany([
+  //   {
+  //     description: 'Clean the house',
+  //     completed: true
+  //   },
+  //   {
+  //     description: 'Go grocery shopping',
+  //     completed: false
+  //   },
+  //   {
+  //     description: 'Do the dishes',
+  //     completed: true
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log("Unable to insert tasks.")
+  //   }
+  //
+  //   console.log(result.insertedIds)
+  // })
 })
 
