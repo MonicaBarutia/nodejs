dddfd1193c6a4af52de97fab45a2c708f6e1c067
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-09 11:07:34 +0300

    Data validation and sanitization.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -9,15 +10,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is not valid!')
+      }
+    }
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number!')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (validator.contains(value, 'password', {ignoreCase: true})) {
+        throw new Error('Password cannot contain the word "password"!')
+      }
+    }
   }
 })
 
 // const me = new User({
-//   name: "Monica",
-//   age: 25
+//   name: "   Monica  ",
+//   email: "MYEMAIL@MONICA.RO    ",
+//   password: 'monica1995'
 // })
 //
 // me.save().then(() => {
@@ -28,16 +60,19 @@ const User = mongoose.model('User', {
 
 const Task = mongoose.model('Task', {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false,
   }
 })
 
 const task1 = new Task({
-  description: "Clean the house",
-  completed: true
+  description: "Do the dishes       ",
+  // completed: true
 })
 
 task1.save().then(() => {
