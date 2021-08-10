0c9aae9bcfefa6be0b0fba31a40968833a5dc9c5
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-10 11:11:36 +0300

    Log in users.

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -4,5 +4,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  autoIndex: true
 })
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -10,12 +10,17 @@ const userSchema =  new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
-    validate(value) {
+    async validate(value) {
       if (!validator.isEmail(value)) {
         throw new Error('Email is not valid!')
+      }
+      const user = await User.findOne( {email: value} )
+      if (user) {
+        throw new Error('User with email already exists!')
       }
     }
   },
@@ -41,6 +46,23 @@ const userSchema =  new mongoose.Schema({
   }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email})
+
+  if (!user) {
+    throw new Error('Unable to login!')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch){
+    throw new Error('Unable to login!')
+  }
+
+  return user
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this
 
