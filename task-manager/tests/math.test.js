4ad096be9e4d2b70824ed00e6e7674beffeacba5
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-12 17:31:48 +0300

    Write tests and assertions.

--- task-manager/tests/math.test.js
+++ task-manager/tests/math.test.js
@@ -1,7 +1,21 @@
-test('Hello world!', () => {
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require("../src/math")
 
+test('Should calculate total with tip', () => {
+  const total = calculateTip(10, .3)
+  expect(total).toBe(13)
 })
 
-test('This should fail', () => {
-  throw new Error('Failure')
+test('Should calculate total with default tip', () => {
+  const total = calculateTip(10)
+  expect(total).toBe(12.5)
+})
+
+test('Should convert 77 F to 25 C', () => {
+  const celsius = fahrenheitToCelsius(77)
+  expect(celsius).toBe(25)
+})
+
+test('Should convert 25 C to 77 F', () => {
+  const fahrenheit = celsiusToFahrenheit(25)
+  expect(fahrenheit).toBe(77)
 })
