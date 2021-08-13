aed9377c31e504cc4534669ed5ab44e954a53bbe
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-13 15:40:29 +0300

    Add fixtures. Finish user tests.

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -91,3 +91,33 @@ test('Should not delete account for unautheticated user', async () => {
     .send()
     .expect(401)
 })
+
+test('Should upload avatar image', async () => {
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'Jean'
+    }).expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.name).toBe('Jean')
+})
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Baia Mare'
+    }).expect(400)
+})
