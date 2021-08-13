894898c969c234813000dd7157237c2557d0d202
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-13 11:51:24 +0300

    Add jest teardown and default user.

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+  name: 'Mike',
+  email: 'mike@example.com',
+  password: 'pass1234'
+}
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
 
 test('Should sign up a new user', async () => {
   await request(app).post('/users').send({
@@ -7,4 +19,18 @@ test('Should sign up a new user', async () => {
     email: 'monica@example.com',
     password: 'MyPass777!'
   }).expect(201)
+})
+
+test('Should login existing user', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: userOne.password
+  }).expect(200)
+})
+
+test('Should not login nonexistent user', async () => {
+  await request(app).post('/user/login').send({
+    email: 'mona@example.com',
+    password: 'red25!!'
+  }).expect(404)
 })
